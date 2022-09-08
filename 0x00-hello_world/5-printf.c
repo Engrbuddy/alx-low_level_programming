0104a768f869fbd49d21e8dd4c54e0c20388417b
ikenna udechukwu <ikeude4u@gmail.com>
2022-09-08 16:30:57 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,3 @@
-Vi 5-printf.c
-
 #include <stdio.h>
 /**
  *  * main-entry point for the program.
@@ -10,3 +8,4 @@ int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
+}
